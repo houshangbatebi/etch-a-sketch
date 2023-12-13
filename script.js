5f929d78ed47188346d3cb28487365e8d9106300
houshangbatebi <109485943+houshangbatebi@users.noreply.github.com>
2023-12-13 17:52:53 +0330

    Create 16*16 grid of square divs using javascript

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const Container = document.createElement('div')
     
+    for (let i = 1; i <= 16; i++) {
+        const flexContainer = document.createElement('div')
+        flexContainer.classList.add('flex-container')
+        for (let j = 1; j <= 16; j++) {
+            const flexItem = document.createElement('div')
+            flexItem.classList.add('flex-item')
+            flexContainer.appendChild(flexItem)
+        }
+        Container.appendChild(flexContainer)
+    }
+
+    document.body.appendChild(Container)
 })
