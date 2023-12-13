0b0a28a5670e6f625e6de152ddad320c74ef9c0f
houshangbatebi <109485943+houshangbatebi@users.noreply.github.com>
2023-12-13 18:32:57 +0330

    Change color of each square when its touched
    To handle the hover/mouseenter event, an event listener is registered for each square.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const Container = document.createElement('div')
+    const container = document.createElement('div')
     
+    // Create 16x16 grid of square divs
     for (let i = 1; i <= 16; i++) {
+        // Create a flex container
         const flexContainer = document.createElement('div')
         flexContainer.classList.add('flex-container')
+
+        // Create 16 flex item for each flex container
         for (let j = 1; j <= 16; j++) {
             const flexItem = document.createElement('div')
             flexItem.classList.add('flex-item')
             flexContainer.appendChild(flexItem)
         }
-        Container.appendChild(flexContainer)
+        // Add this flex container to the parent container
+        container.appendChild(flexContainer)
     }
 
-    document.body.appendChild(Container)
+    // Add container to the page body
+    document.body.appendChild(container)
+
+    // Handle hover/mouseover event for each square
+    const squares = container.querySelectorAll('.flex-item')
+    squares.forEach((square) => {
+        // Add event listener for 'mouseenter' event for each square
+        square.addEventListener('mouseenter', (event) => {
+            // Change the background color of this square
+            const touchedSquare = event.target
+            touchedSquare.style.backgroundColor = 'black'
+        })
+    })
 })
